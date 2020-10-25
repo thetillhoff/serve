6fd59206b56cd37e35637ae23a584c08968619d3
Till Hoffmann <till.hoffmann@enforge.de>
2020-10-26 00:55:58 +0100

    added serve.go & taskfile

---
+++ serve.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fs := http.FileServer(http.Dir("."))
+	http.Handle("/", fs)
+
+	fmt.Println("Listening on :3000...")
+	err := http.ListenAndServe("127.0.0.1:3000", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
